54d7a30ca95ffc98e1e61ef59149929a078db71f
Rain-Chen <214042114@qq.com>
2014-12-04 21:19:56 +0800

    添加用户对象

---
+++ src/main/java/com/autoedu/bean/User.java
@@ -0,0 +1,96 @@
+package com.autoedu.bean;
+
+import java.util.UUID;
+
+/**
+ * Created with Intellij IDEA.
+ * <p>Created by rainchen
+ * User: rainchen
+ * Date: 14-12-4
+ * Time: 下午3:17.
+ * 登录所使用的User对象
+ */
+
+public class User {
+
+    private String id;//用户id采用UUID生成，方便以后数据库集群这个看项目的具体情况
+    private String username;//用户名
+    private String password;//密码
+    private String salt; //加密密码的盐
+    public User() {
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public String getCredentialsSalt(){
+        return username + salt;
+    }
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+
+        User user = (User) o;
+
+        if (id != null ? !id.equals(user.id) : user.id != null) return false;
+        if (password != null ? !password.equals(user.password) : user.password != null) return false;
+        if (salt != null ? !salt.equals(user.salt) : user.salt != null) return false;
+        if (username != null ? !username.equals(user.username) : user.username != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (salt != null ? salt.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", salt='" + salt + '\'' +
+                '}';
+    }
+}
